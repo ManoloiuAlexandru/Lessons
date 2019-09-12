006970a3c6c339d0faae0493be182ab4af746da5
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2019-09-12 15:43:47 +0300

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,88 +1,92 @@
 qestion = input("Creat condition")
 qestion = qestion.split()
 sol = ''
-if "nu" not in qestion:
+if "not" not in qestion:
     interv = list(qestion[-1])
+    listnr = qestion[-1].replace('(', '').replace(')', '').replace('[', '').replace(']', '')
+    listnr = listnr.split(',')
     if interv[0] == '(' and interv[1] != '-':
-        sol = sol + "(" + qestion[0] + '>' + interv[1] + ')'
+        sol = sol + "(" + qestion[0] + '>' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[3] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[3] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[3] + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[3] + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
     elif interv[0] == '(' and interv[1] == '-':
-        sol = sol + "(" + qestion[0] + '>' + interv[1] + interv[2] + ')'
+        sol = sol + "(" + qestion[0] + '>' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[4] + interv[5] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[4] + interv[5] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
     elif interv[0] == '[' and interv[1] != '-':
-        sol = sol + "(" + qestion[0] + '>=' + interv[1] + interv[2] + ')'
+        sol = sol + "(" + qestion[0] + '>=' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[3] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[3] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[3] + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[3] + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
     elif interv[0] == '[' and interv[1] == '-':
-        sol = sol + "(" + qestion[0] + '>=' + interv[1] + interv[2] + ')'
+        sol = sol + "(" + qestion[0] + '>=' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[4] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<' + interv[4] + interv[5] + ')'
+            sol = sol + '&&' + '(' + 'x' + '<' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '&&' + '(' + 'x' + '<=' + interv[4] + interv[5] + ')'
-elif 'nu' in qestion:
+            sol = sol + '&&' + '(' + 'x' + '<=' + listnr[1] + ')'
+elif 'not' in qestion:
     interv = list(qestion[-1])
+    listnr = qestion[-1].replace('(', '').replace(')', '').replace('[', '').replace(']', '')
+    listnr = listnr.split(',')
     if interv[0] == '(' and interv[1] != '-':
-        sol = sol + "(" + qestion[0] + '<=' + interv[1] + ')'
+        sol = sol + "(" + qestion[0] + '<=' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[3] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[3] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[3] + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[3] + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
     elif interv[0] == '(' and interv[1] == '-':
-        sol = sol + "(" + qestion[0] + '<=' + interv[1] + interv[2] + ')'
+        sol = sol + "(" + qestion[0] + '<=' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[4] + interv[5] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[4] + interv[5] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
     elif interv[0] == '[' and interv[1] != '-':
-        sol = sol + "(" + qestion[0] + '<=' + interv[1] + ')'
+        sol = sol + "(" + qestion[0] + '<=' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[3] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[3] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[3] + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[3] + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
     elif interv[0] == '[' and interv[1] == '-':
-        sol = sol + "(" + qestion[0] + '<=' + interv[1] + interv[2] + ')'
+        sol = sol + "(" + qestion[0] + '<=' + listnr[0] + ')'
         if interv[-1] == ')' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] != '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[4] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
         elif interv[-1] == ')' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>=' + interv[4] + interv[5] + ')'
+            sol = sol + '||' + '(' + 'x' + '>=' + listnr[1] + ')'
         elif interv[-1] == ']' and interv[-2] == '-':
-            sol = sol + '||' + '(' + 'x' + '>' + interv[4] + interv[5] + ')'
+            sol = sol + '||' + '(' + 'x' + '>' + listnr[1] + ')'
 print(sol)
