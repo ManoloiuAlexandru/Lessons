344d82372c4bbf5d477a1482b808383c4f69971e
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-01-04 21:04:30 +0200

    Update First_problem.cpp

--- First_problem.cpp
+++ First_problem.cpp
@@ -1,15 +1,14 @@
 /*In a tree are white globes, twice as many red globes and green globes as 3 less then the number of red globes.
 How many globes are there?
-
 Solution in C++:*/
 #include<iostream>
 using namespace std;
 int main()
 {
-    long long a,gv,gr;
-    cin>>a;
-    gr=a*2;
-    gv=gr-3;
-    a=a+gr+gv;
-    cout<<a;
+    long long white_globes, green_globes, red_globes,total=0;
+    cin >> white_globes;
+    red_globes = white_globes * 2;
+    green_globes = red_globes - 3;
+    white_globes = white_globes + red_globes + green_globes;
+    cout << total;
 }
