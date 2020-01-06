d784d3b73accaff4bcea1d3f88c9ecb2cca3754d
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-01-06 13:49:54 +0200

    Update DoubleLinkedList.py

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -1,3 +1,4 @@
+'''https://en.wikipedia.org/wiki/Doubly_linked_list'''
 class DoubleNod:
     def __init__(self, data):
         self.data = data
@@ -9,28 +10,38 @@ class DoubleLinkedList:
     def __init__(self):
         self.head = None
 
-    def addnod(self, data):
+    def add_new_nod_to_end(self, data):
         if self.head is None:
             self.head = DoubleNod(data)
         else:
-            temp = self.head
-            while temp.next:
-                temp = temp.next
+            current_nod = self.head
+            while current_nod.next:
+                current_nod = current_nod.next
             new_nod = DoubleNod(data)
-            temp.next = new_nod
-            new_nod.back = temp
+            current_nod.next = new_nod
+            new_nod.back = current_nod
 
-    def printlist(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" ")
-            temp = temp.next
+    def print_list_inorder(self):
+        current_nod = self.head
+        while current_nod:
+            print(current_nod.data, end=" ")
+            current_nod = current_nod.next
 
-    def printback(self):
-        temp = self.head
-        while temp.next:
-            temp = temp.next
+    def print_list_backwards(self):
+        current_nod = self.head
+        while current_nod.next:
+            current_nod = current_nod.next
 
-        while temp:
-            print(temp.data, end=" ")
-            temp = temp.back
+        while current_nod:
+            print(current_nod.data, end=" ")
+            current_nod = current_nod.back
+
+
+if __name__ == '__main__':
+    new_list = DoubleLinkedList()
+    new_list.add_new_nod_to_end(1)
+    new_list.add_new_nod_to_end(2)
+    new_list.add_new_nod_to_end(3)
+    new_list.print_list_inorder()
+    print()
+    new_list.print_list_backwards()
