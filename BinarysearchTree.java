41fe7a459397e2076c0c31abd10c1f3abc2067f2
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-01-09 15:07:01 +0200

    Create BinarysearchTree.java

---
+++ BinarysearchTree.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class BinaryschTree{
+	int value;
+	BinaryschTree left;
+	BinaryschTree right;
+	
+	public BinaryschTree(int value)
+	{
+		this.value=value;
+		this.left=null;
+		this.right=null;
+	}
+	
+	public BinaryschTree addleaf(BinaryschTree nod,int value)
+	{
+		if (nod==null)
+		{
+			nod=new BinaryschTree(value);
+		}
+		else if (value>nod.value && nod.right==null)
+		{
+			nod.right=new BinaryschTree(value);
+		}
+		else if (value<nod.value && nod.left==null)
+		{
+			nod.left=new BinaryschTree(value);
+		}
+		else if (value>nod.value && nod.right!=null)
+		{
+			return this.addleaf(nod.right, value);
+		}
+		else
+		{
+			return this.addleaf(nod.left, value);
+		}
+		return null;
+	}
+}
