4edf06482ac14d29e9718c0159199581faa164a9
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-02-16 16:53:12 +0200

    Delete First_problem.py

--- First_problem.py
+++
@@ -1,9 +0,0 @@
-'''In a tree are white globes, twice as many red globes and green globes as 3 less then the number of red globes.
-How many globes are there?
-Solution in Python:'''
-
-white_globes = int(input())
-red_globes = white_globes * 2
-green_globes = red_globes - 3
-total_globes = white_globes + red_globes + green_globes
-print(total_globes)
