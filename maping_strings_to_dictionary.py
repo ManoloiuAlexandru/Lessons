5fb5d0c3b3ac3b4627da0c7c51ee9c3587a848f2
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-03-08 11:31:38 +0200

    Create maping_strings_to_dictionary.py

---
+++ maping_strings_to_dictionary.py
@@ -0,0 +1,54 @@
+'''Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+Return the string formed after mapping.
+
+It's guaranteed that a unique mapping will always exist.
+https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/'''
+
+
+def freqAlphabets(s):
+    d = {'1': 'a',
+         '2': 'b',
+         '3': 'c',
+         '4': 'd',
+         '5': 'e',
+         '6': 'f',
+         '7': 'g',
+         '8': 'h',
+         '9': 'i',
+         '10#': 'j',
+         '11#': 'k',
+         '12#': 'l',
+         '13#': 'm',
+         '14#': 'n',
+         '15#': 'o',
+         '16#': 'p',
+         '17#': 'q',
+         '18#': 'r',
+         '19#': 's',
+         '20#': 't',
+         '21#': 'u',
+         '22#': 'v',
+         '23#': 'w',
+         '24#': 'x',
+         '25#': 'y',
+         '26#': 'z'}
+
+    final_string = ''
+    i = len(s) - 1
+    while i >= 0:
+        if s[i] == '#':
+            current_string = s[i - 2:i + 1]
+            i = i - 3
+        else:
+            current_string = s[i]
+            i -= 1
+        final_string = final_string + d[current_string]
+    final_string = final_string[::-1]
+    return final_string
+
+
+if __name__ == '__main__':
+    print(freqAlphabets("10#11#12"))
