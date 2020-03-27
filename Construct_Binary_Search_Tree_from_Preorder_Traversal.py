ff95607dbafeb624844c951c30ff3b50f671a57d
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-03-27 16:15:44 +0200

    Update Construct_Binary_Search_Tree_from_Preorder_Traversal.py

--- Construct_Binary_Search_Tree_from_Preorder_Traversal.py
+++ Construct_Binary_Search_Tree_from_Preorder_Traversal.py
@@ -2,7 +2,10 @@
 
 (Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val,and any descendant of node.right has a value > node.val.  
 Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
-https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/ '''
+https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/ 
+
+The time complexity of this program is: O(n)
+'''
 
 class TreeNode:
     def __init__(self, x):
