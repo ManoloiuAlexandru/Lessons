d81f5482de8562ac2cab37f159985a2a22a881a5
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-07-22 18:00:39 +0300

    Update word_rank.py

--- word_rank.py
+++ word_rank.py
@@ -1,18 +1,23 @@
 ''' Write a program that gets from input a sentence, counts word frequencies, and prints one line for each
 word, in descending order of frequency '''
 
+#Dictionary for the words
 word_rank = dict()
 
-
+#Function that populates the dictionary like this:
+#{word:how many times the words appeared in the sentence}
 def count_word(word, sentence):
     word_rank[word] = sentence.count(word)
 
-
+#The sentence
 text = input()
+#Split the sentence in a list of words
 words_in_text = text.split()
 
+#For each word in the sentence applay the count_word function
 for word in words_in_text:
     count_word(word, text)
+
 
 sorted_keys = sorted(word_rank.items(), key=lambda kv: kv[1], reverse=True)
 for key in sorted_keys:
