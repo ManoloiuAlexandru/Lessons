2f107a42de86c37360af5fb4692449d5e079d200
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-07-24 18:45:45 +0300

    Update move_zero.py

--- move_zero.py
+++ move_zero.py
@@ -1,10 +1,16 @@
-'''Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-(https://leetcode.com/problems/move-zeroes/)'''
+"""Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+(https://leetcode.com/problems/move-zeroes/)"""
 
 
 def move_zeros(nums):
+    """
+    function that takes the list as param and moves the 0 by calling the builtin functions append and remove
+    :return
+    the final list
+    """
     n = len(nums)
     i = 0
+    # iterate through the list
     while i < n:
         if nums[i] == 0:
             nums.append(0)
