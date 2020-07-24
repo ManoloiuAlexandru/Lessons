915347adab664bb27b93d71c98572f3e6136cfea
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-07-24 15:53:39 +0300

    Update priorityqueue.py

--- priorityqueue.py
+++ priorityqueue.py
@@ -1,15 +1,33 @@
+"""
+https://www.geeksforgeeks.org/priority-queue-set-1-introduction/
+"""
 
-class PriorityQueue():
+
+# Declaration of class PriorityQueue
+class PriorityQueue:
+
     def __init__(self):
+        """
+        class constructor
+        """
         self.queue = []
 
     def isEmpty(self):
+        """
+        method that checks if the queue is empty and returns True if it is empty or False otherwise
+        """
         return len(self.queue) == []
 
     def insert(self, data):
+        """
+        method that adds new data to the queue
+        """
         self.queue.append(data)
 
     def delete(self):
+        """
+        method that deletes the max element(the element with the highest priority) from the list
+        """
         maxel = 0
         for i in range(0, len(self.queue)):
             if self.queue[i] > self.queue[maxel]:
@@ -19,12 +37,14 @@ class PriorityQueue():
         return item
 
 
-prioq = PriorityQueue()
-prioq.insert(1)
-prioq.insert(5)
-prioq.insert(3)
-prioq.insert(4)
-print(prioq.delete())
-print(prioq.delete())
-print(prioq.delete())
-print(prioq.delete())
+if __name__ == '__main__':
+    prioq = PriorityQueue()
+    prioq.insert(1)
+    prioq.insert(5)
+    prioq.insert(3)
+    prioq.insert(4)
+    print(prioq.delete())
+    print(prioq.delete())
+    print(prioq.delete())
+    print(prioq.isEmpty())
+    print(prioq.delete())
