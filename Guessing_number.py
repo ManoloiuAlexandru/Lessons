1868fad3288cfd694e267dc537f82ab9a8e0e526
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-07-27 17:23:12 +0300

    Update Guessing_number.py

--- Guessing_number.py
+++ Guessing_number.py
@@ -1,9 +1,9 @@
-'''
+"""
 In the function we are using binary search to guess a number from the interval [low,high]. The reason we use binary search it is because it reaches a solution after at most k=1+[log(high-low+1)].
 In computer science, binary search, also known as half-interval search, logarithmic search, or binary chop, is a search algorithm that finds the position of a target value within a sorted array.
-
 https://www.geeksforgeeks.org/complexity-analysis-of-binary-search/
-'''
+"""
+
 
 def guess(n, low, high):
     turns = 0
@@ -17,3 +17,7 @@ def guess(n, low, high):
         else:
             high = mid + 1
     return turns
+
+
+if __name__ == '__main__':
+    print(guess(2899, 1, 100000))
