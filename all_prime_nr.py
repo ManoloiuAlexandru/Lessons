3860a130e21b994a77cf3e2212b2e13f775aa548
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-07-28 18:39:44 +0300

    Update all_prime_nr.py

--- all_prime_nr.py
+++ all_prime_nr.py
@@ -1,23 +1,22 @@
-'''Given a number n, print all primes smaller than or equal to n.
-
+"""Given a number n, print all primes smaller than or equal to n.
 Example:
-
 Input : n =10
-Output : 2 3 5 7 
+Output : 2 3 5 7
+Input : n = 20
+Output: 2 3 5 7 11 13 17 19"""
 
-Input : n = 20 
-Output: 2 3 5 7 11 13 17 19'''
 
-
-def prime(nr):
-    for div in range(2, nr // 2 + 1):
-        if nr % div == 0:
+def prime(number):
+    for div in range(2, number // 2 + 1):
+        if number % div == 0:
             return False
     return True
 
 
-given_nr = int(input())
+if __name__ == '__main__':
 
-for nr in range(2, given_nr):
-    if prime(nr) is True:
-        print(nr, end=" ")
+    given_nr = int(input())
+
+    for nr in range(2, given_nr):
+        if prime(nr) is True:
+            print(nr, end=" ")
