2651c02ca6c7805deb656096cee695d776fa638c
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-08-02 13:52:55 +0300

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,21 @@
+"""
+Write a program that prints on each line the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number, and for multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz".
+"""
+
+
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+
+
+if __name__ == '__main__':
+    fizz_buzz()
