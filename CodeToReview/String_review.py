ccc2efbc955169b48dcaad45d62af4b2d8bd84ee
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-04 17:50:43 +0300

    Create String_review.py

---
+++ CodeToReview/String_review.py
@@ -0,0 +1,7 @@
+"""
+I want to see each word from my string on each line
+"""
+my_string = "I am Deathwing, the Destroyer, the end of all things! Inevitable. Indomitable. I am the Cataclysm!"
+
+for word in my_string:
+    print(my_string)
