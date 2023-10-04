442afed5921f36b86751ea914a5935cfda81ebf9
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-04 16:55:56 +0300

    Create Password.py

---
+++ Classes_template/Password.py
@@ -0,0 +1,10 @@
+class Password:
+    def __init__(self, password_contains):
+        self.password_contains = password_contains
+        self.security = None
+
+    def check_security(self):
+        """
+        Check if the password is strong,week or medium based on the general standards
+        """
+        pass
