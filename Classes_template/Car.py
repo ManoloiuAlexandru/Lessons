85a8681a260d55f84076e4a516e9d4eb364c4879
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-04 16:49:58 +0300

    Create Car.py

---
+++ Classes_template/Car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, mark, engine, year, km, price):
+        self.mark = mark
+        self.engine = engine
+        self.year = year
+        self.km = km
+        self.price = price
